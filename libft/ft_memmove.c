4eb70f5e9550781a679030f6f26ccc7a03a18be0
amsteradri <101824703+amsteradri@users.noreply.github.com>
2022-10-10 20:12:46 +0200

    Add files via upload

---
+++ libft/ft_memmove.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memmove.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adgutier <adgutier@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 12:53:40 by adgutier          #+#    #+#             */
+/*   Updated: 2022/09/23 13:22:15 by adgutier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, int len)
+{
+	int		i;
+	char	*deesti;
+	char	*source;
+
+	i = 0;
+	deesti = (char *)dst;
+	source = (char *)src;
+	if (deesti > source)
+	{
+		while (len--)
+			deesti[len] = source[len];
+	}
+	else if (deesti < source)
+		ft_memcpy(dst, src, len);
+	return (dst);
+}
+
+/*
+#include <unistd.h>
+
+void	*ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int		count;
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		ft_putchar(str[count]);
+		count++;
+	}
+}
+
+
+int main(void)
+{
+	char str[50]="bferfcwefwfw";
+	char str1[50]="basic la chupa";
+	
+
+	ft_memmove(str, str1, 3);
+	ft_putstr(str);
+}*/
